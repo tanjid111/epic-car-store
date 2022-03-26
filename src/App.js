1dbf3a258566a3ce30b809f1171d4c497876f974
tanjid111 <tanjidhossain@gmail.com>
2022-03-26 04:55:21 -0700

    final4

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div>
       <Header></Header>
       <Shop></Shop>
-      {/* <QuestionAnswer></QuestionAnswer> */}
+      <QuestionAnswer></QuestionAnswer>
     </div>
   );
 }
